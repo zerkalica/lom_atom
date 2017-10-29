c6ec81f1100660f4c5337d62c630485e4255d4ee
Stefan Zerkalica <zerkalica@gmail.com>
2017-10-29 14:33:43 +0300

    exposed force constants, removed props decorator

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 // @flow
 
 export {default as Atom} from './Atom'
-export {default as mem, props, memkey, detached, force, action} from './mem'
+export {default as mem, memkey, detached, force, action} from './mem'
 export {defaultContext, BaseLogger, ConsoleLogger} from './Context'
 
 export type {
     ResultOf,
     IAtom,
+    IAtomForce,
     IAtomStatus,
     IContext,
     IAtomHandler,
     NamesOf,
     ILoggerStatus,
     ILogger
+} from './interfaces'
+
+export {
+    ATOM_FORCE_NONE,
+    ATOM_FORCE_CACHE,
+    ATOM_FORCE_UPDATE
 } from './interfaces'
